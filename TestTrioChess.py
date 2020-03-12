153106b5bc91988075b8807cf1e2b19d4d09e08f
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-03-12 01:30:42 -0400

    Update TestTrioChess.py

--- TestTrioChess.py
+++ TestTrioChess.py
@@ -1,12 +1,15 @@
 import unittest
 
-from trial_refactor import *
+#from trial_refactor import *
 
 class TestTrioChess(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testTrial(self): 
         self.assertEqual(4, 4)
+
+    def testTrial2(self): 
+        self.assertEqual(5, 5)
 
 if __name__ == '__main__':
     print('Running unit tests')
